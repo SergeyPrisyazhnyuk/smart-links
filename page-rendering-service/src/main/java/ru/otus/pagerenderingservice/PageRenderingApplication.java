c6448c6a85c04a4be09b4f22b02701d75ff33529
SergeyPrisyazhnyuk <vasilisk811@yandex.ru>
2025-08-13 19:41:01 +0300

    feat : added page-rendering-service iter1

---
+++ page-rendering-service/src/main/java/ru/otus/pagerenderingservice/PageRenderingApplication.java
@@ -0,0 +1,11 @@
+package ru.otus.pagerenderingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PageRenderingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PageRenderingApplication.class, args);
+    }
+}
